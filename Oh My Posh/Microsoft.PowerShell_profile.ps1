41ae0a29c21c3beacd493c6f079b1388e7620ddf
Jonathan Nash <github@capoala.me>
2024-03-30 23:19:28 +0000

    Updated PowerShell Profile sample with the URL now that it's published.

--- Oh My Posh/Microsoft.PowerShell_profile.ps1
+++ Oh My Posh/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,2 @@
-oh-my-posh init pwsh --config "$($env:POSH_THEMES_PATH)\custom.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/Capoala/Themes-for-things/main/Oh%20My%20Posh/custom.omp.json' | Invoke-Expression
 $PSStyle.FileInfo.Directory = ""
